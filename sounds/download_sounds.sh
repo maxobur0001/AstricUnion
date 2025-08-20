cbfb7f778427858e4b189818784b3059f34cdf2d
maxobur0001 <msblock09@gmail.com>
2025-08-20 19:36:53 +0300

    Added sounds from Astro striker (also script)

---
+++ sounds/download_sounds.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+wget -o "SoundUrl.mp3" "https://www.dl.dropboxusercontent.com/scl/fi/n9tzb3v1vs6x1fayy9cig/saberamb.mp3?rlkey=usn1ur6e34g8aiuhbjie7wn0a&st=g37ivi7q&dl=1"; sleep 5
+wget -o "SliceURL.mp3" "https://www.dl.dropboxusercontent.com/scl/fi/wcrnvlb0ktu7grn9zlbm2/SwordSlice.mp3?rlkey=b33e9n7e417ippeacujix850i&st=6cb19dsz&dl=1"; sleep 5
+wget -o "SliceURL2.mp3" "https://www.dl.dropboxusercontent.com/scl/fi/7t1s9x07liqpn7pqb9kev/Slice2.mp3?rlkey=f0bnje8jgsxz8m5alqf9gkaug&st=oewbmq79&dl=1"; sleep 5
+wget -o "SliceURL3.mp3" "https://www.dl.dropboxusercontent.com/scl/fi/7t1s9x07liqpn7pqb9kev/Slice2.mp3?rlkey=f0bnje8jgsxz8m5alqf9gkaug&st=oewbmq79&dl=1"; sleep 5
+wget -o "Cloakon.mp3" "https://www.dl.dropboxusercontent.com/scl/fi/rwt1izlwcxung6ld0yu2g/cloak.mp3?rlkey=l3u7a2fzx5wuq2q2w30fx90xv&st=81b2e77p&dl=1"; sleep 5
+wget -o "Cloakoff.mp3" "https://www.dl.dropboxusercontent.com/scl/fi/qjo7opc89zal4t3jdqe6z/uncloak.mp3?rlkey=cbubq79ora1vn6bhnkvws4jhi&st=kgfw94f7&dl=1"; sleep 5
+
+wget -o "ChargeUp.mp3" "https://www.dl.dropboxusercontent.com/scl/fi/9g8sguici6u5vrqx35wpe/ChargeupGUN.mp3?rlkey=mvevntjbjc5y2ht0xmvac88kc&st=5iba01im&dl=1"; sleep 5
+wget -o "ChargeDown.mp3" "https://www.dl.dropboxusercontent.com/scl/fi/1qm1f5qofnjcxi3nusuub/Uncharge.mp3?rlkey=09grm6xyyi86933gtxc4yxb03&st=oa4crhkh&dl=1"; sleep 5
+wget -o "Blast1.mp3" "https://www.dl.dropboxusercontent.com/scl/fi/1hdis324ztuy26ybm99f2/AstrSHOOT1.mp3?rlkey=f3nzk6gz4qnsxcnr7dwvdos0w&st=vkytslx6&dl=1"; sleep 5
+wget -o "Blast2.mp3" "https://www.dl.dropboxusercontent.com/scl/fi/b1qscdkrzj8gbl4p6xq87/AstrSHOOT2.mp3?rlkey=nuxpfbcy7sqm8mvn9br8uzfa8&st=j1c3m4fi&dl=1"; sleep 5
+wget -o "Blast3.mp3" "https://www.dl.dropboxusercontent.com/scl/fi/h1xyaqywr5evww4f6maeb/AstrSHOOT3.mp3?rlkey=yqbjrknjewjyl8j52mp3ks601&st=9bpxfxck&dl=1"; sleep 5
+
+wget -o "SoundUrl5.mp3" "https://www.dl.dropboxusercontent.com/scl/fi/i3us2xj0q47tccze51ymm/ramattack.mp3?rlkey=xy5xulfzaq7nf8fzzvo21z29f&st=g83wealc&dl=1"; sleep 5
+
+wget -o "CRITCHARGEON.mp3" "https://www.dl.dropboxusercontent.com/scl/fi/tjw2jwosp6vrrtze04gc4/Critpower-ON.mp3?rlkey=5s17b2awbnvpggve4wruwiyrv&st=8wldp4jx&dl=1"; sleep 5
+wget -o "CRITCHARGEONLOOP.mp3" "https://www.dl.dropboxusercontent.com/scl/fi/esth3yhjgr2d4y33pzpsp/critcharged.mp3?rlkey=hkakdupi9m7xppul62m9tudsd&st=4ws2it4j&dl=1"; sleep 5
+wget -o "CRITCHARGEOFF.mp3" "https://www.dl.dropboxusercontent.com/scl/fi/upnwtsxwdop7hnqzzjr6r/critoff.mp3?rlkey=ekmn7fx0ayhsbetcs4exzufdk&st=qn56v72y&dl=1"; sleep 5
+
